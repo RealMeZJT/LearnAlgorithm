7b74d29bc1f46c33d42a62382cc79a44a2193128
Zhou Jiatao <jiatao_zhou@163.com>
2018-02-05 20:05:53 +0800

    添加参考资料

--- Swift/BubbleSort.playground/Contents.swift
+++ Swift/BubbleSort.playground/Contents.swift
@@ -1,6 +1,7 @@
 //: Playground - noun: a place where people can play
 
 // 冒泡排序
+// 参考资料：https://zh.wikipedia.org/wiki/%E5%86%92%E6%B3%A1%E6%8E%92%E5%BA%8F
 
 func bubbleSort(arr: inout Array<Int>) {
     for i in 0..<arr.count - 1 {
